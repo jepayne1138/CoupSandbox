bfc460b3ff46225309d1d41ff5db2680f336f035
jepayne <jepayne1138@gmail.com>
2016-03-17 01:25:48 -0500

    Fixed fragments so main game fragment loads properly

--- app/src/main/java/com/jepaynedev/coupsandbox/CoupActivity.java
+++ app/src/main/java/com/jepaynedev/coupsandbox/CoupActivity.java
@@ -1,12 +1,30 @@
 package com.jepaynedev.coupsandbox;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class CoupActivity extends AppCompatActivity {
 
+    public GameManager game;
+    final private FragmentManager fragmentManager = getSupportFragmentManager();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        game = new GameManager();
+
+        // Create the game instance
+        game.setCurrentPlayer(game.addPlayer("Jim"));
+        game.addPlayer("Casey");
+        game.addPlayer("Caitlin");
+        game.addPlayer("Adrian");
+        game.addPlayer("Ian");
+
+        GameFragment gameFragment = new GameFragment();
+
+        fragmentManager.beginTransaction().add(R.id.fragment_container, gameFragment).commit();
     }
 }
--- app/src/main/java/com/jepaynedev/coupsandbox/GameFragment.java
+++ app/src/main/java/com/jepaynedev/coupsandbox/GameFragment.java
@@ -1,18 +1,24 @@
 package com.jepaynedev.coupsandbox;
 
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ListView;
+
+import com.jepaynedev.coupsandbox.databinding.FragmentGameBinding;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class GameFragment extends Fragment {
+public class GameFragment extends Fragment implements View.OnClickListener {
 
+    private GameManager game;
 
     public GameFragment() {
         // Required empty public constructor
@@ -22,8 +28,36 @@ public class GameFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        CoupActivity activity = (CoupActivity)getActivity();
+        game = activity.game;
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game, container, false);
+        FragmentGameBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_game, container, false);
+
+        // Set the current user to the data model in the binding
+        binding.setCurPlayer(game.getCurrentPlayer());
+
+        // Initialize the player list view
+        ListView playerListView = (ListView) binding.getRoot().findViewById(R.id.listPlayers);
+        PlayerListAdapter playerListAdapter = new PlayerListAdapter(game, getActivity());
+        playerListView.setAdapter(playerListAdapter);
+
+        // Set buttons to use this interface as the onClick listener
+        ((ImageButton)(binding.getRoot().findViewById(R.id.buttonCoinUp))).setOnClickListener(this);
+        ((ImageButton)(binding.getRoot().findViewById(R.id.buttonCoinDown))).setOnClickListener(this);
+
+        return binding.getRoot();
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.buttonCoinUp:
+                game.getCurrentPlayer().incrementCoins();
+                break;
+            case R.id.buttonCoinDown:
+                game.getCurrentPlayer().decrementCoins();
+                break;
+        }
+    }
 }
